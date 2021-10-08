fdd25616702e955592bff88366d36f3c57f56564
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-10-08 15:10:15 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'VAT Number Validation',
+    'summary': """VAT validation for Partner's VAT numbers.""",
+    'description': """VAT validation for Partner's VAT numbers.""",
+    'version': '14.0.0.1',
+    'category': 'Accounting/Accounting',
+    'depends': [
+        'base_vat',
+    ],
+    'data': [],
+    'application': True,
+}
